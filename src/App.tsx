72fe6c118f4e2dba7de71f1312e26e69309bc532
Kevin Agyeman <kevinagyemann@gmail.com>
2023-09-23 23:16:27 +0200

    console errore

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { ThemeProvider } from './components/ui/theme-provider';
 import Index from './pages';
 
 function App() {
   return (
     <>
-      <Index />
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <Index />
+      </ThemeProvider>
     </>
   );
 }
--- src/components/projects-list.component.tsx
+++ src/components/projects-list.component.tsx
@@ -1,6 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { projectService } from '../services/project.service';
 import { ProjectData } from '../types/project-schema';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 const ProjectsList = () => {
   const [projects, setProjects] = useState<ProjectData[]>();
@@ -20,6 +28,19 @@ const ProjectsList = () => {
   }, []);
   return (
     <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
       {projects?.map((project: ProjectData, index: number) => (
         <p key={index}>
           <br />
