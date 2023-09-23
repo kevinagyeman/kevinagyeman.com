b3762e591986d31575db1195e28e26bdfc4c7836
Kevin Agyeman <kevinagyemann@gmail.com>
2023-09-24 00:20:09 +0200

    it works

--- src/components/projects-list.component.tsx
+++ src/components/projects-list.component.tsx
@@ -9,6 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { ModeToggle } from './ui/mode-toggle';
 
 const ProjectsList = () => {
   const [projects, setProjects] = useState<ProjectData[]>();
@@ -28,28 +29,23 @@ const ProjectsList = () => {
   }, []);
   return (
     <>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-
-      {projects?.map((project: ProjectData, index: number) => (
-        <p key={index}>
-          <br />
-          {project.id}
-          <br />
-          {project.title}
-          <br />
-        </p>
-      ))}
+      <ModeToggle />
+      <div className='container grid lg:grid-cols-3'>
+        {projects?.map((project: ProjectData, index: number) => (
+          <Card key={index} className='m-4'>
+            <CardHeader>
+              <CardTitle>{project.title}</CardTitle>
+              <CardDescription></CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ const Index = () => {
   return (
     <>
       <ProjectsList />
-      <ProjectsAdd />
     </>
   );
 };
