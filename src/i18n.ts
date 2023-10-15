1cede98e685b00322cc68578e598d0d1936739d5
Kevin Agyeman <kevinagyemann@gmail.com>
2023-10-15 15:40:21 +0200

    translation

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 // don't want to use this?
 // have a look at the Quick start guide
 // for passing in lng and translations on init
@@ -27,9 +27,9 @@ i18n
       escapeValue: false, // not needed for react as it escapes by default
     },
 
-    backend: {
-      loadPath: "./src/assets/i18n/{{lng}}.json",
-    },
+    // backend: {
+    //   loadPath: "./public/i18n/{{lng}}.json",
+    // },
   });
 
 export default i18n;
