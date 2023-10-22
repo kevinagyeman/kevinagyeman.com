1d80beea31ac16a08694a445346329273e714c28
Kevin Agyeman <kevinagyemann@gmail.com>
2023-10-22 02:19:45 +0200

    fix build

--- src/components/project/projects-add.component.tsx
+++ src/components/project/projects-add.component.tsx
@@ -1,15 +1,12 @@
-import { projectsListState } from "@/store/projects-store";
-import { useState } from "react";
-import { useSetRecoilState } from "recoil";
+import { projectDataState, projectsListState } from "@/store/projects-store";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { projectService } from "../../services/project.service";
 import { ProjectSchema } from "../../types/project-schema";
 import { Button } from "../ui/button";
 import ProjectForm from "./project-form.component";
 
 export default function ProjectsAdd() {
-  const [project, setProject] = useState<ProjectSchema>({
-    title: "",
-  });
+  const [project, setProject] = useRecoilState<ProjectSchema>(projectDataState);
   const setProjects = useSetRecoilState<ProjectSchema[]>(projectsListState);
 
   const addProject = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -30,12 +27,7 @@ export default function ProjectsAdd() {
 
   return (
     <>
-      <ProjectForm
-        isDisabled={false}
-        projectSetter={setProject}
-        submitFunction={addProject}
-        project={project}
-      />
+      <ProjectForm isDisabled={false} projectSetter={setProject} submitFunction={addProject} project={project} />
       <Button type="submit" className="mt-3 w-full" form="form">
         Crea Progetto
       </Button>
