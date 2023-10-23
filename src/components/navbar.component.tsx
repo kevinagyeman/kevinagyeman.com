96f388b6acf7bff920389782d8baef9cb1016814
Kevin Agyeman <kevinagyemann@gmail.com>
2023-10-23 20:52:29 +0200

    patch logo

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,6 +1,4 @@
 import flagItaly from "@/assets/img/italy.png";
-import logoBlack from "@/assets/img/logo-dark.svg";
-import logoWhite from "@/assets/img/logo-light.svg";
 import flagUK from "@/assets/img/united-kingdom.png";
 import { useTheme } from "@/components/ui/theme-provider";
 import { auth } from "@/firebase";
@@ -8,7 +6,7 @@ import i18n from "@/i18n";
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Moon, Sun } from "lucide-react";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { isAdminLoggedDataState } from "../store/admin-store";
@@ -24,6 +22,7 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectVa
 
 export default function Navbar() {
   const isAdminLoggedData = useRecoilValue<boolean>(isAdminLoggedDataState);
+
   const navigation = isAdminLoggedData
     ? [
         { name: "Home", href: "/" },
@@ -38,11 +37,6 @@ export default function Navbar() {
         { name: "Contacts", href: "/#contacts" },
       ];
 
-  const [logoSrc, setLogoSrc] = useState<string>();
-
-  useEffect(() => {
-    window.document.documentElement.classList.contains("dark") ? setLogoSrc(logoWhite) : setLogoSrc(logoBlack);
-  }, []);
   return (
     <Disclosure as="nav" className="sticky top-0 z-50 mb-5 border-b bg-white/80 backdrop-blur-sm dark:bg-zinc-950/80">
       {({ open }) => (
@@ -60,11 +54,11 @@ export default function Navbar() {
                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
-                <ThemeChanger logoSetter={setLogoSrc} />
+                <ThemeChanger />
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img className="h-8 w-auto" src={logoSrc} alt="Logo" />
+                  <Logo />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
@@ -78,7 +72,7 @@ export default function Navbar() {
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <div className="hidden sm:block">
-                  <ThemeChanger logoSetter={setLogoSrc} />
+                  <ThemeChanger />
                 </div>
                 <LanguageSelector />
                 <IconAdmin />
@@ -108,20 +102,14 @@ export default function Navbar() {
   );
 }
 
-type ThemeChangerProps = {
-  logoSetter: Dispatch<SetStateAction<string | undefined>>;
-};
-
-const ThemeChanger = ({ logoSetter }: ThemeChangerProps) => {
-  const { setTheme } = useTheme();
+const ThemeChanger = () => {
+  const { theme, setTheme } = useTheme();
 
   const changeTheme = () => {
-    if (window.document.documentElement.classList.contains("dark")) {
+    if (theme === "dark") {
       setTheme("light");
-      logoSetter(logoBlack);
     } else {
       setTheme("dark");
-      logoSetter(logoWhite);
     }
   };
 
@@ -219,5 +207,24 @@ const LanguageSelector = () => {
         </SelectGroup>
       </SelectContent>
     </Select>
+  );
+};
+
+const Logo = () => {
+  const { theme } = useTheme();
+
+  return (
+    <svg
+      id="Livello_1"
+      className="h-8 w-auto"
+      fill={theme === "dark" ? "white" : "black"}
+      data-name="Livello 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1080 1080"
+    >
+      <title>logo-k</title>
+      <path d="M1044.35,1079.5H781.48a35.12,35.12,0,0,1-24.86-10.3L540,852.58,323.38,1069.2a35.12,35.12,0,0,1-24.86,10.3H35.65a35.15,35.15,0,0,1-24.85-60L959.48,70.8H793.75L298.93,565.63a35.15,35.15,0,0,1-60-24.86V35.65A35.15,35.15,0,0,1,274.07.5H466.64a35.15,35.15,0,0,1,35.15,35.15V263.34L754.1,11A35.81,35.81,0,0,1,779.19.5h265.16a35.15,35.15,0,0,1,24.85,60L120.52,1009.2H284L645.5,647.66a35.14,35.14,0,0,1,49.35-.35l76.74,74.57.36.35,297.25,297.26a35.15,35.15,0,0,1-24.85,60ZM796,1009.2H959.48L722.41,772.12l-51.7-50.25-81,81ZM309.22,70.8V455.91L431.49,333.64V70.8Z" />
+      <path d="M1043.9,781.93a35.58,35.58,0,0,1-25.19-10.43L812.39,565.18a35.61,35.61,0,0,1,0-50.35L834,493.23h0L1018.71,308.5a35.61,35.61,0,0,1,60.79,25.18V746.32a35.6,35.6,0,0,1-35.6,35.61ZM887.93,540l120.35,120.36V419.64Z" />
+    </svg>
   );
 };
